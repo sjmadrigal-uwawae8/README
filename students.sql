1a23dc722e9eb3687af1d4cc9096a5dcbc09e968
Mary Thecia Kasandra Valmoria <mtkmvalmoria@cmc.pshs.edu.ph>
2026-05-04 11:27:26 +0800

    students.sql-vagm

---
+++ students.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE students(
+  id TEXT PRIMARY KEY,
+  first_name TEXT Not NULL,
+  middle_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  gender TEXT NOT NULL,
+  birthdate TEXT NOT NULL,
+  place_of_birth TEXT NOT NULL,
+  email_address UNIQUE NOT NULL,
+  contact_number TEXT NOT NULL,
+  section TEXT NOT NULL,
+  league_color TEXT NOT NULL
+)
